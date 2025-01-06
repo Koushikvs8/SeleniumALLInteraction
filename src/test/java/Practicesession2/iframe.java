7d1572465bb74c90f8c5d69325da6baf8ceac47c
Koushikvs8 <koushikvittalshet@gmail.com>
2025-01-06 20:49:00 +0530

    ddd

--- src/test/java/Practicesession2/iframe.java
+++ src/test/java/Practicesession2/iframe.java
@@ -1,8 +1,13 @@
 package Practicesession2;
 
+import java.io.File;
 import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,11 +21,39 @@ public class iframe {
 			     driver=new ChromeDriver();
 			     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			     driver.manage().window().maximize();
-			     driver.get("https://practice.expandtesting.com/iframe");
-			     driver.switchTo().frame("mce_0_ifr");
-			     WebElement ddd= driver.findElement(By.xpath("//p[normalize-space()='Your content goes here.']"));
-			     ddd.clear();
-			   ddd.sendKeys("koushik you can do it");
-			     driver.quit();
-		}      
+			     driver.get("https://testautomationpractice.blogspot.com/");
+			     driver.findElement(By.xpath("//a[text()='Blogger']")).click();
+			     swictchto();
+			     driver.findElement(By.xpath("//header[@class=\"hero--header\"]//a[contains(text(),'Create your blog')][1]")).click();
+		}    
+		
+		
+		
+		public void takess()
+		{    TakesScreenshot ts=(TakesScreenshot)driver;
+			 File sourcefile=   ts.getScreenshotAs(OutputType.FILE);
+			 File targetfile=new File("C:\\Users\\User\\eclipse-workspace\\SeleniumFRAmwork\\ALLAutomationinteractions\\test-output"+System.currentTimeMillis()+"ss.png");
+			 sourcefile.renameTo(targetfile);
+			
+		}
+		public void swictchto()
+		{     String parent=driver.getWindowHandle() ;     
+			  Set<String> windows=driver.getWindowHandles();
+			  Iterator< String> window=windows.iterator();
+			   
+			  
+			while(window.hasNext()) 
+			  {
+				  if(!window.next().equals(parent) )
+				  {
+				  driver.switchTo().window(window.next());
+				  }
+				
+			}
+			 
+			  
+		}
+		
+		
+		
 }
--- src/test/java/Utilities/DataProviders.java
+++ src/test/java/Utilities/DataProviders.java
@@ -5,12 +5,12 @@ import org.testng.annotations.DataProvider;
 public class DataProviders {
 /// Data providerby object arry //////
 	
-	@DataProvider(name="ddt")
+	@DataProvider(name="ddt" )
 	public Object[][] Data()
 	{
-		Object[][] data= {{"koushik", "gfgfhjg"}
-		//{"swathi1233", "gfgfhjg"},
-		//{"swfhjfj", "gfgfhjg"}
+		Object[][] data= {{"koushik", "gfgfhjg"},
+		{"swathi1233", "gfgfhjg"},
+		{"swfhjfj", "gfgfhjg"}
 		};
 		
 		
